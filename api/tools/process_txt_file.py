549f8fa68c379b7e6580409c45d5f237a3edb03f
Akash Singh <ak45hhere@gmail.com>
2024-03-04 15:29:41 +0530

    Refactor file writing methods in TxtFile class

--- api/tools/process_txt_file.py
+++ api/tools/process_txt_file.py
@@ -1,41 +1,23 @@
 class TxtFile:
-    def add_q_to_file(text):
-        filename = "temp/" + "qna.txt"
+
+    def add_to_file(filename, text):
         text = text + "\n\n"
 
         with open(filename, "a+") as file:
             file.write(text)
         return True
+
+    def add_q_to_file(text):
+        return TxtFile.add_to_file("temp/qna.txt", text)
 
     def add_a_to_file_openai(text):
-        print(text)
-        filename = "temp/" + "ans.txt"
-        text = text + "\n\n"
-
-        with open(filename, "a+") as file:
-            file.write(text)
-        return True
+        return TxtFile.add_to_file("temp/ans.txt", text)
 
     def add_a_to_file_gemini(text):
-        filename = "temp/" + "ans_gemini.txt"
-        text = text + "\n\n"
-
-        with open(filename, "a+") as file:
-            file.write(text)
-        return True
+        return TxtFile.add_to_file("temp/ans_gemini.txt", text)
 
     def add_a_to_file_gemini_img(text):
-        filename = "temp/" + "ans_gemini_img.txt"
-        text = text + "\n\n"
-
-        with open(filename, "a+") as file:
-            file.write(text)
-        return True
+        return TxtFile.add_to_file("temp/ans_gemini_img.txt", text)
 
     def add_t_to_file_typer(text):
-        filename = "temp/" + "typer.txt"
-        text = text + "\n\n"
-
-        with open(filename, "a+") as file:
-            file.write(text)
-        return True
+        return TxtFile.add_to_file("temp/typer.txt", text)
